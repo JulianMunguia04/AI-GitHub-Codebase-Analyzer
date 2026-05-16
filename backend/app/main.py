eaf20865051e355376284520a50ce05874b80dee
JulianMunguia04 <julianmunguia04@gmail.com>
2026-05-16 17:07:33 -0600

    Search query parameter api endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Flask is working!"
+
+@app.route("/health")
+def health_check():
+    return {"status": 200}
+
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+
+    return f"You searched for: {query}"
 
 if __name__ == "__main__":
     app.run(debug=True)
