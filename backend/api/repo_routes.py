cf81fb7112733ce166cd30673a73c5219c5e1868
JulianMunguia04 <julianmunguia04@gmail.com>
2026-06-21 01:16:28 -0600

    Content for each file in nested tree

--- backend/api/repo_routes.py
+++ backend/api/repo_routes.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, request, jsonify
 
-from backend.services.github_service import get_repo_metadata, get_repo_tree
+from backend.services.github_service import get_repo_metadata, get_repo_tree, get_file_content
 from backend.utils.tree_builder import build_tree
 
 repo_routes = Blueprint("repo_routes", __name__)
 
+ALLOWED_EXTENSIONS = {
+    ".py", ".js", ".ts", ".html", ".css",
+    ".java", ".go", ".cpp", ".c", ".md"
+}
+
+def is_code_file(filename):
+    return any(filename.endswith(ext) for ext in ALLOWED_EXTENSIONS)
 
 @repo_routes.route("/repo/tree", methods=["GET"])
 def get_repo_full_tree():
@@ -26,6 +33,24 @@ def get_repo_full_tree():
         return {"error": "Failed to fetch tree"}, 500
 
     nested_tree = build_tree(tree)
+
+    def inject_content(node, path=""):
+        for name, value in list(node.items()):
+            current_path = f"{path}/{name}" if path else name
+
+            # file node
+            if isinstance(value, dict) and value.get("type") == "blob":
+                if is_code_file(name):
+                    file_data = get_file_content(owner, repo, current_path)
+
+                    if file_data:
+                        node[name]["content"] = file_data["content"]
+
+            # folder
+            elif isinstance(value, dict):
+                inject_content(value, current_path)
+
+    inject_content(nested_tree)
 
     return jsonify({
         "repo": metadata["name"],
