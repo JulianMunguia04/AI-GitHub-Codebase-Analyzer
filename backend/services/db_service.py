a69f20f5c5a93ddc6c482070f8901fc6f2dc382d
JulianMunguia04 <julianmunguia04@gmail.com>
2026-07-03 22:43:23 -0600

    Database Connection

---
+++ backend/services/db_service.py
@@ -0,0 +1,13 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
+)
+
+def get_db():
+    return conn
